0c02bfe8357e4ea1e40bd11e03cce293541c2272
junseongday <junseongday@gmail.com>
2020-02-23 20:53:55 +0900

    스프링5 Functional Bean 등록

--- src/main/java/me/whiteship/demospring51/Demospring51Application.java
+++ src/main/java/me/whiteship/demospring51/Demospring51Application.java
@@ -1,13 +1,34 @@
 package me.whiteship.demospring51;
 
+import me.whiteship.out.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Demospring51Application {
 
+    @Autowired
+    MyService service;
+
     public static void main(String[] args) {
-        SpringApplication.run(Demospring51Application.class, args);
+
+//        SpringApplication.run(Demospring51Application.class, args);
+
+        //java8
+        SpringApplication app = new SpringApplication(Demospring51Application.class);
+        app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) context -> {
+            context.registerBean(MyService.class);
+            context.registerBean(ApplicationRunner.class, () -> (ApplicationRunner) args1 -> System.out.println("Functional Bean Definition!!"));
+        });
+        app.run(args);
     }
 
 }
---
+++ src/main/java/me/whiteship/out/MyService.java
@@ -0,0 +1,7 @@
+package me.whiteship.out;
+
+import org.springframework.stereotype.Service;
+
+//@Service
+public class MyService {
+}
