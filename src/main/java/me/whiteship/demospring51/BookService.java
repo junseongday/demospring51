32d281b9ccba2f2151035b21ac760a8eee758dc9
junseongday <junseongday@gmail.com>
2020-02-23 18:46:25 +0900

    Autowired 할때 주입받는 빈이 여러개경우 1

---
+++ src/main/java/me/whiteship/demospring51/BookService.java
@@ -0,0 +1,17 @@
+package me.whiteship.demospring51;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    @Qualifier("junseongBookRepository")
+    BookRepository bookRepository;
+
+    public void printBookRepository() {
+        System.out.println(bookRepository.getClass());
+    }
+}
