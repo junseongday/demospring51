be6e3adcff45e55c18a9876c4a08c075e4fe8c39
junseongday <junseongday@gmail.com>
2020-02-23 21:30:19 +0900

    Scope ProxcyMode 말고 다른방법

--- src/main/java/me/whiteship/demospring51/Proto.java
+++ src/main/java/me/whiteship/demospring51/Proto.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Scope(value = "prototype")
 public class Proto {
 
     @Autowired
